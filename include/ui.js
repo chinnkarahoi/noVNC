chinnkarahoi/noVNC

commit 529c64e1036b622ff971b1a0cd13856837d3a9eb

Re-arrange global UI variables

include/ui.js
@@ -24,20 +24,25 @@ var UI;
     UI = {
 
         rfb_state: 'loaded',
+
         resizeTimeout: null,
+        popupStatusTimeout: null,
+        hideKeyboardTimeout: null,
+
         settingsOpen: false,
         connSettingsOpen: false,
-        popupStatusTimeout: null,
         clipboardOpen: false,
         keyboardVisible: false,
-        hideKeyboardTimeout: null,
-        lastKeyboardinput: null,
-        defaultKeyboardinputLen: 100,
-        extraKeysVisible: false,
-        ctrlOn: false,
-        altOn: false,
+
         isTouchDevice: false,
         rememberedClipSetting: null,
+        lastKeyboardinput: null,
+        defaultKeyboardinputLen: 100,
+
+        shiftDown: false,
+        ctrlDown: false,
+        altDown: false,
+        altGrDown: false,
 
         // Setup rfb object, load settings from browser storage, then call
         // UI.init to setup the UI/menus

commit 046a1b0decc5b0d2bdbdcba6d4d5ed1a79cc93a0

Fix the extra keys
Commit 529c64e1036b622ff971b1a0cd13856837d3a9eb broke this. #shame#

include/ui.js
@@ -33,6 +33,7 @@ var UI;
         connSettingsOpen: false,
         clipboardOpen: false,
         keyboardVisible: false,
+	extraKeysVisible: false,
 
         isTouchDevice: false,
         isSafari: false,
@@ -40,10 +41,8 @@ var UI;
         lastKeyboardinput: null,
         defaultKeyboardinputLen: 100,
 
-        shiftDown: false,
-        ctrlDown: false,
-        altDown: false,
-        altGrDown: false,
+        ctrlOn: false,
+        altOn: false,
 
         // Setup rfb object, load settings from browser storage, then call
         // UI.init to setup the UI/menus