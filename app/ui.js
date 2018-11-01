chinnkarahoi/noVNC

commit 61f93180c8cbdc6259d0b083b51f169053f66f8c

Show/hide mouse button selector in updateViewOnly
Before this commit, the mouse button selector was hidden when the user
clicked it in view only..

app/ui.js
@@ -1607,6 +1607,7 @@ var UI = {
             document.getElementById('noVNC_toggle_extra_keys_button')
                 .classList.remove('noVNC_hidden');
         }
+        UI.setMouseButton(1); //has it's own logic for hiding/showing
     },
 
     updateLogging: function() {

commit 2342d3f726055112274e505d0f116d8a3b224c80

Dont reset touch mouse button state for view-only
Fixes bug introduced in 61f93180c8cbdc6259d0b083b51f169053f66f8c.

app/ui.js
@@ -1575,13 +1575,16 @@ const UI = {
                 .classList.add('noVNC_hidden');
             document.getElementById('noVNC_toggle_extra_keys_button')
                 .classList.add('noVNC_hidden');
+            document.getElementById('noVNC_mouse_button' + UI.rfb.touchButton)
+                .classList.add('noVNC_hidden');
         } else {
             document.getElementById('noVNC_keyboard_button')
                 .classList.remove('noVNC_hidden');
             document.getElementById('noVNC_toggle_extra_keys_button')
                 .classList.remove('noVNC_hidden');
+            document.getElementById('noVNC_mouse_button' + UI.rfb.touchButton)
+                .classList.remove('noVNC_hidden');
         }
-        UI.setMouseButton(1); //has it's own logic for hiding/showing
     },
 
     updateShowDotCursor() {