chinnkarahoi/noVNC

commit f796b05e42cfac7044cca9603e59f258605228f3

Add support for URL fragment parameters
Passing parameters as part of the fragment could be considered
benifical from a security or privacy standpoint when compared to query
string parameters. The URL fragment parameters are not sent to the
server.

app/webutil.js
@@ -20,10 +20,19 @@ export function initLogging(level) {
 }
 
 // Read a query string variable
+// A URL with a query parameter can look like this (But will most probably get logged on the http server):
+// https://www.example.com?myqueryparam=myvalue
+//
+// For privacy (Using a hastag #, the parameters will not be sent to the server)
+// the url can be requested in the following way:
+// https://www.example.com#myqueryparam=myvalue&password=secreatvalue
+//
+// Even Mixing public and non public parameters will work:
+// https://www.example.com?nonsecretparam=example.com#password=secreatvalue
 export function getQueryVar(name, defVal) {
     "use strict";
     const re = new RegExp('.*[?&]' + name + '=([^&#]*)'),
-        match = document.location.href.match(re);
+        match = ''.concat(document.location.href, " ", window.location.hash).match(re);
     if (typeof defVal === 'undefined') { defVal = null; }
 
     if (match) {

vnc_lite.html
@@ -109,13 +109,20 @@
         // query string. If the variable isn't defined in the URL
         // it returns the default value instead.
         function readQueryVariable(name, defaultValue) {
-            // A URL with a query parameter can look like this:
+            // A URL with a query parameter can look like this (But will most probably get logged on the http server):
             // https://www.example.com?myqueryparam=myvalue
+            //
+            // For privacy (Using a hastag #, the parameters will not be sent to the server)
+            // the url can be requested in the following way:
+            // https://www.example.com#myqueryparam=myvalue&password=secreatvalue
+            //
+            // Even Mixing public and non public parameters will work:
+            // https://www.example.com?nonsecretparam=example.com#password=secreatvalue
             //
             // Note that we use location.href instead of location.search
             // because Firefox < 53 has a bug w.r.t location.search
             const re = new RegExp('.*[?&]' + name + '=([^&#]*)'),
-                  match = document.location.href.match(re);
+                  match = ''.concat(document.location.href, " ", window.location.hash).match(re);
 
             if (match) {
                 // We have to decode the URL since want the cleartext value

commit 5e409c9631d4a7ab53fc64f2b779622771376cd7

Fix parsing of query string variables
This space that was added here was added to the parsed value of the
query variable. This broke any comparisons with the value, for example
"myvar=true" resulted in a value of "true ".

This was broken by f796b05e42cfac7044cca9603e59f258605228f3

The commit also adds unit tests for webutil.getConfigVar() that will
detect problems like this in the future.

app/webutil.js
@@ -32,7 +32,7 @@ export function initLogging(level) {
 export function getQueryVar(name, defVal) {
     "use strict";
     const re = new RegExp('.*[?&]' + name + '=([^&#]*)'),
-        match = ''.concat(document.location.href, " ", window.location.hash).match(re);
+        match = ''.concat(document.location.href, window.location.hash).match(re);
     if (typeof defVal === 'undefined') { defVal = null; }
 
     if (match) {

vnc_lite.html
@@ -122,7 +122,7 @@
             // Note that we use location.href instead of location.search
             // because Firefox < 53 has a bug w.r.t location.search
             const re = new RegExp('.*[?&]' + name + '=([^&#]*)'),
-                  match = ''.concat(document.location.href, " ", window.location.hash).match(re);
+                  match = ''.concat(document.location.href, window.location.hash).match(re);
 
             if (match) {
                 // We have to decode the URL since want the cleartext value

tests/test.webutil.js
@@ -7,6 +7,48 @@ import * as WebUtil from '../app/webutil.js';
 describe('WebUtil', function () {
     "use strict";
 
+    describe('config variables', function () {
+        it('should parse query string variables', function () {
+            // history.pushState() will not cause the browser to attempt loading
+            // the URL, this is exactly what we want here for the tests.
+            history.pushState({}, '', "test?myvar=myval");
+            expect(WebUtil.getConfigVar("myvar")).to.be.equal("myval");
+        });
+        it('should return default value when no query match', function () {
+            history.pushState({}, '', "test?myvar=myval");
+            expect(WebUtil.getConfigVar("other", "def")).to.be.equal("def");
+        });
+        it('should handle no query match and no default value', function () {
+            history.pushState({}, '', "test?myvar=myval");
+            expect(WebUtil.getConfigVar("other")).to.be.equal(null);
+        });
+        it('should parse fragment variables', function () {
+            history.pushState({}, '', "test#myvar=myval");
+            expect(WebUtil.getConfigVar("myvar")).to.be.equal("myval");
+        });
+        it('should return default value when no fragment match', function () {
+            history.pushState({}, '', "test#myvar=myval");
+            expect(WebUtil.getConfigVar("other", "def")).to.be.equal("def");
+        });
+        it('should handle no fragment match and no default value', function () {
+            history.pushState({}, '', "test#myvar=myval");
+            expect(WebUtil.getConfigVar("other")).to.be.equal(null);
+        });
+        it('should handle both query and fragment', function () {
+            history.pushState({}, '', "test?myquery=1#myhash=2");
+            expect(WebUtil.getConfigVar("myquery")).to.be.equal("1");
+            expect(WebUtil.getConfigVar("myhash")).to.be.equal("2");
+        });
+        it('should prioritize fragment if both provide same var', function () {
+            history.pushState({}, '', "test?myvar=1#myvar=2");
+            expect(WebUtil.getConfigVar("myvar")).to.be.equal("2");
+        });
+    });
+
+    describe('cookies', function () {
+        // TODO
+    });
+
     describe('settings', function () {
 
         describe('localStorage', function () {