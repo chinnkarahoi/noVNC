b02915a663327a0ae0686a8ccb1de04a606be9a5
chinnkarahoi <chinnkarahoi@gmail.com>
2022-04-13 12:32:20 +0800

    revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /app || exit 1
 VNC_ADDRESS=${VNC_ADDRESS:-localhost:5900}
-PULSE_ADDRESS=${PULSE_ADDRESS:-localhost:1234}
-/app/vncproxy --static /app -vncAddress $VNC_ADDRESS -udpAddress $PULSE_ADDRESS
+/app/vncproxy --static /app -vncAddress $VNC_ADDRESS
