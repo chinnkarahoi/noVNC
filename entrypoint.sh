300f2dade520ab59944a41781fba5773836b02fe
chinnkarahoi <chinnkarahoi@gmail.com>
2022-04-13 12:24:53 +0800

    add PULSE_ADDRESS

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /app || exit 1
 VNC_ADDRESS=${VNC_ADDRESS:-localhost:5900}
-/app/vncproxy --static /app -vncAddress $VNC_ADDRESS
+PULSE_ADDRESS=${PULSE_ADDRESS:-localhost:1234}
+/app/vncproxy --static /app -vncAddress $VNC_ADDRESS -udpAddress $PULSE_ADDRESS
