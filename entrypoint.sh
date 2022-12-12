6b7b81d8837c52d978b53f848764a428c4e0e4d6
chinnkarahoi <chinnkarahoi@gmail.com>
2022-12-12 20:53:26 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /app || exit 1
 VNC_ADDRESS=${VNC_ADDRESS:-localhost:5900}
-/app/vncproxy --static /app -vncAddress $VNC_ADDRESS
+PROXY_PORT=${PROXY_PORT:-8888}
+/app/vncproxy --address :${PROXY_PORT} --static /app -vncAddress $VNC_ADDRESS
